76c1cc9a37698ee18f804024f4696a2c3fbf9a03
ErikMazgon <em8703@student.uni-lj.si>
2022-05-16 10:36:40 +0200

    maps in to

--- app/src/main/java/si/uni_lj/fe/tnuv/kinospored/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/kinospored/MainActivity.java
@@ -2,7 +2,10 @@ package si.uni_lj.fe.tnuv.kinospored;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Jebenjo", Toast.LENGTH_SHORT).show();
+
+        configureNextButton();
+    }
+
+    private void configureNextButton() {
+        Button nextButtton = (Button) findViewById(R.id.nextButton);
+        nextButtton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Maps.class));
+            }
+        });
     }
 }
